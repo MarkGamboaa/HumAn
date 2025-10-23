889b205fe1da5ee1e63ad448bb8b875c22f9ccc4
Mark Gerard G. Gamboa <gamboamarkgerard@gmail.com>
2025-10-23 19:00:03 +0800

    Initial Commit

---
+++ HumAn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HumAn
+//
+//  Created by Micah Marie G. Gamboa on 10/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
